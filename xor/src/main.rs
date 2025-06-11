3fde8096fe489950291f0b08e6fb659ecd39dafe
shu.yamaga <s.yamaga.0318@gmail.com>
2025-06-11 21:30:54 +0900

    oops

--- xor/src/main.rs
+++ xor/src/main.rs
@@ -1,22 +1,36 @@
 use xor::{bits, bool};
 
 fn main() {
-    let a = true;
-    let b = false;
+    let plain_bool = true;
+    let key_bool = false;
 
-    println!("Boolean XOR between {} and {} is {}", a, b, bool::xor(a, b));
+    // encrypt
+    let cipher_bool = bool::xor(plain_bool, key_bool);
+    println!(
+        "Boolean XOR between {} and {} is {}",
+        plain_bool, key_bool, cipher_bool
+    );
     // Output:
     // Boolean XOR between true and false is true
 
-    let c = 0b1101_0011;
-    let d = 0b1010_1000;
+    // decrypt
+    let decrypted_bool = bool::xor(cipher_bool, key_bool);
+    println!(
+        "Decrypted boolean: {:#?} using key {:#?} is {}",
+        cipher_bool, key_bool, decrypted_bool
+    );
+    // Output:
+    // Decrypted boolean: true using key false is true
 
+    let plain_bits = 0b1101_0011;
+    let key_bits = 0b1010_1000;
+
+    // encrypt
+    let cipher_bits = bits::xor(plain_bits, key_bits);
     println!(
         "Bits XOR between {:08b} and {:08b} is {:08b}",
-        c,
-        d,
-        bits::xor(c, d)
-    )
+        plain_bits, key_bits, cipher_bits
+    );
     // Output:
     // Bits XOR between 11010011 and 10101000 is 01111011
 
