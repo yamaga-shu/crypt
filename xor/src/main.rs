bf79c0aaf34281255e85d5e19c47a6b01f0a8b02
shu.yamaga <s.yamaga.0318@gmail.com>
2025-06-11 21:10:19 +0900

    Update: add Output comment

--- xor/src/main.rs
+++ xor/src/main.rs
@@ -5,9 +5,18 @@ fn main() {
     let b = false;
 
     println!("Boolean XOR between {} and {} is {}", a, b, bool::xor(a, b));
+    // Output:
+    // Boolean XOR between true and false is true
 
     let c = 0b1101_0011;
     let d = 0b1010_1000;
 
-    println!("Bits XOR between {} and {} is {}", c, d, bits::xor(c, d))
+    println!(
+        "Bits XOR between {:08b} and {:08b} is {:08b}",
+        c,
+        d,
+        bits::xor(c, d)
+    )
+    // Output:
+    // Bits XOR between 11010011 and 10101000 is 01111011
 }
